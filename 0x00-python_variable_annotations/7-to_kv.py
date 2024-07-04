f55a41e55cecfc2e0e2a3f268728252bf9d4ffd8
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-04 16:42:00 +0300

    Convert to kv

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module provides a utility function to_kv that takes a key and a value.
+The function returns a tuple with the key and the square of the value.
+"""
+
+from typing import Union
+
+def to_kv(k: str, v: Union[int, float]) -> tuple:
+    """
+    Return a tuple with the key and the square of the value.
+
+    Args:
+        k (str): The key to be included in the tuple.
+        v (Union[int, float]): The value to be squared and included in the tuple.
+
+    Returns:
+        tuple: A tuple containing the key and the square of the value.
+    """
+    return (k, v**2)
