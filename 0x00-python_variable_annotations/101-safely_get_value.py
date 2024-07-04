f20a452080bb0efbbe4695bf8325d62abd138178
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-04 20:31:14 +0300

    Safely get the value

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+This module provides a function
+'safely_get_value' that attempts to retrieve a value
+from a mapping using a specified key.
+If the key is not found, it returns a default value.
+"""
+
+from typing import Union, Sequence, Any, Mapping, TypeVar
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(
+    dct: Mapping[Any, T],
+    key: Any,
+    default: Union[T, None] = None
+) -> Union[T, None]:
+    """
+    Attempt to retrieve a value from a mapping using a key,
+    or return a default value.
+
+    Args:
+        dct (Mapping[Any, T]): The mapping from which to retrieve the value.
+        key (Any): The key to look up in the mapping.
+        default (Union[T, None]): The default value
+        to return if the key is not found.
+
+    Returns:
+        Union[T, None]: The value associated with
+        the key, or the default value.
+    """
+    return dct.get(key, default)
