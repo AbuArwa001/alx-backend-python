c701d807e7961e1efa54da1ddeb6e28d62f6c859
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-04 16:02:48 +0300

    Cumulative culculation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 from typing import List
+
 """
-Module to Define a function that
-Calculate the sum of all elements
+Module to define a function that calculates the sum of
+all elements in a list of floats.
 """
 
 
 def sum_list(input_list: List[float]) -> float:
     """
-    Calculate the sum of all elements
+    Calculate the sum of all elements in a list of floats.
+
+    Args:
+        input_list (List[float]): A list of floating point numbers.
+
+    Returns:
+        float: The sum of all elements in the list.
     """
-    total: float = 0
-    for i in input_list:
-        total += i
-    return total
+    return sum(input_list)
