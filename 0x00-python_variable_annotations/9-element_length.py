7e867ef625d65d1e684bc69c1c6a7e5679c9fd21
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-04 19:08:22 +0300

    Annotate List of tuples

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+This module provides a function 'element_length'
+that takes a sequence of sequences
+and returns a list of tuples, each
+containing a sequence and its corresponding length.
+"""
+
+from typing import Callable, Sequence, Tuple, List
+
+
+def element_length(lst: Sequence[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Return a list of tuples with each sequence and its length.
+
+    Args:
+        lst (Sequence[Sequence]): A sequence of sequences.
+
+    Returns:
+        List[Tuple[Sequence, int]]: A list of tuples,
+        each containing a sequence and its length.
+    """
+    return [(i, len(i)) for i in lst]
