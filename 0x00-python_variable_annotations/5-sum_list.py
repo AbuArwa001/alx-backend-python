82a6f60eac4f3aeb4cf2320e0d8cb8fd81218c95
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-04 15:56:05 +0300

    Cumulative culculation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+from typing import List
 """
 Module to Define a function that
 Calculate the sum of all elements
 """
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Calculate the sum of all elements
     """
