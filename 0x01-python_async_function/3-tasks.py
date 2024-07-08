c6d8c7d878b81174b41ff65becbb7fa77083b9d3
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-08 14:57:06 +0300

    Create async io tasks

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -8,7 +8,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Creates an asyncio task that waits for
     a random delay between 0 and max_delay (inclusive).
