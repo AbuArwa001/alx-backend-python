69d97c261fef0dea0e7c73950d26ef2f26919823
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-08 13:11:58 +0300

    Add Hash bang

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """
 This module contains an asynchronous coroutine
 for demonstrating async and await syntax in Python.
@@ -24,6 +26,6 @@ async def wait_random(max_delay: int = 10) -> float:
     Returns:
         float: The actual delay in seconds.
     """
-    delay = random.uniform(0, max_delay)
+    delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
