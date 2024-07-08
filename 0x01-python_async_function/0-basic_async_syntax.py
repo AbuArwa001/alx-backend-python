12f67a0dc6392b3edf8f65cbdbd1a708e53a26a0
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-08 13:00:12 +0300

    Create An asynchronous coroutine that waits for a random delay and return the delay.

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,30 @@
+"""
+This module contains an asynchronous coroutine
+for demonstrating async and await syntax in Python.
+
+The module provides the following function:
+- wait_random: An asynchronous coroutine
+that waits for a random delay and returns the delay.
+
+Functions:
+    wait_random(max_delay: int = 10) -> float
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Waits for a random delay between
+    1 and max_delay seconds (inclusive) and returns the actual delay.
+
+    Args:
+        max_delay (int): The maximum delay in seconds. Default is 10.
+
+    Returns:
+        float: The actual delay in seconds.
+    """
+    delay = random.uniform(1, max_delay)
+    await asyncio.sleep(delay)
+    return delay
