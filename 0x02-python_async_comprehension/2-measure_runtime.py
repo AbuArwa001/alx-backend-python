0185c3f78b8c47fa54b4fb9fbd0251891b307a19
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-09 13:15:13 +0300

    Change gather

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -18,12 +18,9 @@ async def measure_runtime() -> float:
     Returns:
         float: Average execution time per task.
     """
-    start_time: float = time.perf_counter()
+    start_time: float = time.time()
     await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
+        *(async_comprehension() for _ in range(4))
     )
-    end_time: float = time.perf_counter()
+    end_time: float = time.time()
     return end_time - start_time
