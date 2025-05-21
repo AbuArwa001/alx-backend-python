07e4a177ed827d56bb67ee64e3f19febc05ba9b3
AbuArwa001 <khalfanathman12@yahoo.com>
2025-05-21 14:53:31 +0300

    Add asynchronous functions to fetch users from the database

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+3-concurrent.py
+"""
+import aiosqlite
+import asyncio
+import functools
+from typing import List, Dict, Any
+
+async def async_fetch_users():
+    """Asynchronous function to fetch users from the database."""
+    async with aiosqlite.connect('users.db') as conn:
+        cursor = await conn.execute("SELECT * FROM users")
+        rows = await cursor.fetchall()
+        return rows
+async def async_fetch_older_users():
+    """Asynchronous function to fetch users older than 25."""
+    async with aiosqlite.connect('users.db') as conn:
+        cursor = await conn.execute("SELECT * FROM users WHERE age > 40")
+        rows = await cursor.fetchall()
+        return rows
+async def main():
+    data = await asyncio.gather(
+        async_fetch_users(),
+        async_fetch_older_users()
+    )
+    print(data)
+
+asyncio.run(main())
