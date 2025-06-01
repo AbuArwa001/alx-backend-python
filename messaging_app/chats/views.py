cc1160e277cff0e2128f8e2d95677f208396a18f
AbuArwa001 <khalfanathman12@yahoo.com>
2025-06-01 05:32:49 +0300

    Implement CRUD operations for User, Conversation, and Message ViewSets with proper permissions and response handling

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,197 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status, permissions
+from rest_framework.response import Response
+from .models import User, Conversation, Message
+from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing users in the messaging application.
+    Provides CRUD operations for User model.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response(UserSerializer(user).data)
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    def perform_destroy(self, instance):
+        """
+        Delete the user instance.
+        """
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    def list(self, request, *args, **kwargs):
+        """
+        List all users in the messaging application.
+        """
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Retrieve a specific user by ID.
+        """
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+    def get_permissions(self):
+        """
+        Get the permissions for the UserViewSet.
+        """
+        if self.action in ['create', 'update', 'destroy']:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+    def get_serializer_class(self):
+        """
+        Get the serializer class for the UserViewSet.
+        """
+        if self.action in ['create', 'update']:
+            return UserSerializer
+        return UserSerializer
+class ConversationViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing conversations in the messaging application.
+    Provides CRUD operations for Conversation model.
+    """
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        conversation = serializer.save()
+        return Response(ConversationSerializer(conversation).data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        conversation = serializer.save()
+        return Response(ConversationSerializer(conversation).data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        """
+        Delete the conversation instance.
+        """
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def list(self, request, *args, **kwargs):
+        """
+        List all conversations in the messaging application.
+        """
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Retrieve a specific conversation by ID.
+        """
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def get_permissions(self):
+        """
+        Get the permissions for the ConversationViewSet.
+        """
+        if self.action in ['create', 'update', 'destroy']:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+
+    def get_serializer_class(self):
+        """
+        Get the serializer class for the ConversationViewSet.
+        """
+        if self.action in ['create', 'update']:
+            return ConversationSerializer
+        return ConversationSerializer
+class MessageViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing messages in the messaging application.
+    Provides CRUD operations for Message model.
+    """
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        message = serializer.save()
+        return Response(MessageSerializer(message).data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        message = serializer.save()
+        return Response(MessageSerializer(message).data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        """
+        Delete the message instance.
+        """
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def list(self, request, *args, **kwargs):
+        """
+        List all messages in the messaging application.
+        """
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Retrieve a specific message by ID.
+        """
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def get_permissions(self):
+        """
+        Get the permissions for the MessageViewSet.
+        """
+        if self.action in ['create', 'update', 'destroy']:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+
+    def get_serializer_class(self):
+        """
+        Get the serializer class for the MessageViewSet.
+        """
+        if self.action in ['create', 'update']:
+            return MessageSerializer
+        return MessageSerializer
+
+
