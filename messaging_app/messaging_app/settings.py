0492fd5f658b6deaf9a45fe9e08de8dda3b0d4c0
AbuArwa001 <khalfanathman12@yahoo.com>
2025-06-01 06:44:55 +0300

    Refactor URL routing to use NestedDefaultRouter and update settings for token authentication

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    'django_filters'
+    'django_filters',
+    "chats",  # Your app for handling messaging features
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
@@ -54,6 +56,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',  # For browser-based API usage
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',  # Require login by default
